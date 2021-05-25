64d36839f564a31578ab348c54eb3707c5b11f4e
jason <1328222979@qq.com>
2021-05-25 15:41:43 +0800

    衣服

---
+++ main.py
@@ -0,0 +1,45 @@
+id1 = 1
+date1 = 1
+name1 = "羽绒服"
+price1=253.6
+num1 = 500
+quan1 = 10
+
+id2 = 2
+date2 = 2
+name2 = "牛仔裤"
+price2=253.6
+num2 = 500
+quan2 = 10
+
+id3 = 3
+date3 = 3
+name3 = "风衣"
+price3=253.6
+num3 = 500
+quan3 = 10
+
+id4 = 4
+date4 = 4
+name4 = "皮草"
+price4=253.6
+num4 = 500
+quan4 = 10
+
+id5 = 5
+date5 = 5
+name5 = "T恤"
+price5=253.6
+num5 = 500
+quan5 = 10
+
+print("=======================衣服系统=======================)")
+print("编号      日期      名称        价格       数量      销售额")
+print("------------------------------------------------------")
+print("id1      date1    name1     price1     num1      quan1")
+print("id2      date2    name2     price2     num2      quan2")
+print("id3      date3    name3     price3     num3      quan3")
+print("id4      date4    name4     price4     num4      quan4")
+print("id5      date5    name5     price5     num5      quan5")
+print("------------------------------------------------------")
+print("总金额：￥",(price1 * num1 + price2 * num2 + price3 * num3 + price4 * num4 + price5 * num5))
